379af21ddee13b9ae70451293f702b181464b8a9
Ramya.U <ramyayadav2016@gmail.com>
2020-12-08 13:28:53 +0530

    [Ramya] Add . Added method to validate pincode using regex

---
+++ PinCode/UC3-PinCode.js
@@ -0,0 +1,14 @@
+//UC 2 Check for 400088B – this should fail
+//to take input from command line
+const prompt = require('prompt-sync')();
+let pin = prompt('Enter pin: ');
+validatePin(pin);
+function validatePin(pin)
+{
+    //regex for pincode validation
+    let pinRegExp = RegExp("^[0-9]{1}[a-zA-Z0-9]{5}$");
+    if (pinRegExp.test(pin))
+        console.log("Valid pincode")
+    else
+        console.log("Invalid pincode!")
+}
